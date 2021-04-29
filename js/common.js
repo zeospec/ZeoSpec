5da2b6df455c82eb0ddcd8c526243784e9a34fa6
zeospec <zeospec@users.noreply.github.com>
2021-04-30 01:27:26 +0530

    zeospec

--- js/common.js
+++ js/common.js
@@ -64,47 +64,6 @@ $(document).ready(function() {
   $(".page img, .post img").attr("data-action", "zoom");
   $(".page a img, .post a img").removeAttr("data-action", "zoom");
 
-  /* =======================
-  // Instagram Feed with In
-  ======================= */
-  // userId and accessToken from Matthew Elsom (https://codepen.io/matthewelsom/pen/zrrrLN) for example, for which he thanks a lot!
-
-  // var instagramFeed = new Instafeed({
-  //   get: 'user',
-  //   limit: 6,
-  //   resolution: 'standard_resolution',
-  //   userId: '1618198676',
-  //   accessToken: '1618198676.1677ed0.6835939f2435410fa16fbfa86ed645bc',
-  //   template:
-  //     '<li class="instagram-item"><a href="{{link}}" aria-label="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></li>'
-  // });
-
-  // if ($('#instafeed').length) {
-  //   instagramFeed.run();
-  // }
-
-    /* =======================
-  // Instagram Feed 
-  ======================= */
-
-  (function(){
-    new InstagramFeed({
-        'username': 'zeospec',
-        'container': document.getElementById("instafeed"),
-        'display_profile': false,
-        'display_biography': false,
-        'display_gallery': true,
-        // 'display_igtv': true,
-        'callback': null,
-        'styling': true,
-        'items': 6,
-        // 'items_per_row': 3,
-        // 'margin': 1,
-        'lazy_load': true,
-        'on_error': console.error
-    });
-})();
-
 
   /* =======================
   // Scroll Top Button
